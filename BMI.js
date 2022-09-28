5413baf38c013d910df5e19fbf5505fa0fccb9bc
kfhwang <kfhwang@nutc.edu.tw>
2022-09-28 14:44:51 +0800

    20220928

--- BMI.js
+++ BMI.js
@@ -1,7 +1,21 @@
 var readline = require("readline-sync");
-var height = readline.question("Ur height?");
-var weight = readline.question("Ur weight?");
-var hPower2 = Math.pow(height/100,2);
-var bmi = weight/hPower2;
+
+while (true) {
+    var height = readline.question("Ur height?");
+    if (isNaN(height) || height < 10 || height > 220) {
+        console.log("Please input ur height between 10 and 200");
+        continue;
+    }
+
+    var weight = readline.question("Ur weight?");
+    if (isNaN(weight) || weight < 10 || weight > 200) {
+        console.log("Please...");
+        continue;
+    }
+
+}
+
+var hPower2 = Math.pow(height / 100, 2);
+var bmi = weight / hPower2;
 //console.log("Your height: "+ height+ '\n ,weight:'+weight);
-console.log("Your bmi= "+ bmi);
+console.log("Your bmi= " + bmi);
